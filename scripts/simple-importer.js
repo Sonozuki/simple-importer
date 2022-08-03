db311faa391146f856ba1f8e5e18c29fa676a179
Andy Crocker <atmcrocker@gmail.com>
2022-08-03 20:08:10 +0100

    Change hook used to inject custom button into journal directory

--- scripts/simple-importer.js
+++ scripts/simple-importer.js
@@ -1,17 +1,9 @@
-Hooks.on('changeSidebarTab', (directory) => {
-    if (directory.tabName != 'journal')
-        return;
-
-    // ensure the custom button doesn't already exist
-    let journalImportDataButton = directory.element.find('#import-data');
-    if (journalImportDataButton.length > 0)
-        return;
-
+Hooks.on('renderJournalDirectory', (directory, html) => {
     // add custom button
-    let actionButtonsHeaderRow = directory.element.find('div.header-actions');
+    let actionButtonsHeaderRow = html.find('div.header-actions');
     actionButtonsHeaderRow.append(`<button id="import-data"><i class="fas fa-book"></i> Import Journal Data</button>`);
 
-    directory.element.on('click', '#import-data', (event) => {
+    html.on('click', '#import-data', (event) => {
         SimpleImporter.importJournalDataConfig.render(true);
     });
 });
